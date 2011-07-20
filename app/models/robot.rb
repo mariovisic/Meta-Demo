23e1f5a4e65c47d86bb77352972e0783e525ece9
Mario Visic <mario@mariovisic.com>
2011-07-20 23:00:19 +0800

    Lots more seed.

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -3,4 +3,8 @@ class Robot < ActiveRecord::Base
   has_many :robot_parts
   has_many :parts, :through => :robot_parts
 
+  def part_names
+    parts.map(&:name).to_sentence.presence || 'None'
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,34 @@
 require 'lib/factories'
 
 puts '=== Creating Manufacturers === '
-100.times { Factory.create(:manufacturer) }
+10.times { Factory.create(:manufacturer) }
 
 puts '=== Creating Suppliers === '
-200.times { Factory.create(:supplier) }
+20.times { Factory.create(:supplier) }
 
 puts '=== Creating Parts === '
-500.times { Factory.create(:part) }
+50.times { Factory.create(:part) }
 
 puts '=== Creating Robots === '
-1000.times { Factory.create(:robot) }
+100.times { Factory.create(:robot) }
+
+puts '=== Creating Robot Parts === '
+200.times do
+  robot  = Robot.order('RANDOM()').first
+  part   = Part.order('RANDOM()').first
+  RobotPart.create!(:robot => robot, :part => part)
+end
+
+puts '=== Creating Supplier Parts === '
+200.times do
+  supplier = Supplier.order('RANDOM()').first
+  part     = Part.order('RANDOM()').first
+  SupplierPart.create!(:supplier => supplier, :part => part)
+end
+
+puts '=== Creating Supplier Manufacturers === '
+200.times do
+  supplier     = Supplier.order('RANDOM()').first
+  manufacturer = Manufacturer.order('RANDOM()').first
+  SupplierManufacturer.create!(:supplier => supplier, :manufacturer => manufacturer)
+end
